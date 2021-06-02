f6d626074eb8811795d1fa01461c76c635102c2c
Michael Rolli <michael.rolli@unibe.ch>
2021-06-02 17:35:31 +0200

    Turn on code coverage

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -2,6 +2,12 @@ dir = File.expand_path(File.dirname(__FILE__))
 
 Dir["#{dir}/shared_examples/*.rb"].sort.each { |f| require f }
 
+RSpec.configure do |c|
+  c.after(:suite) do
+    RSpec::Puppet::Coverage.report!
+  end
+end
+
 def verify_exact_file_contents(subject, title, expected_lines)
   content = subject.resource('file', title).send(:parameters)[:content]
   expect(content.split("\n").reject { |line| line =~ %r{(^$|^#)} }).to match_array expected_lines
